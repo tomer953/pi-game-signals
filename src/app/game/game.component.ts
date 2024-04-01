b0ad676587bd701218ab7a8e87226710b4f99af8
tmeirov <tmeirov@paloaltonetworks.com>
2024-04-01 23:49:12 +0300

    added hostlistener

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,16 +1,46 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { GameRulesComponent } from "./game-rules/game-rules.component";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+} from '@angular/core';
+import { GameRulesComponent } from './game-rules/game-rules.component';
+import { PI_STR } from './pi';
 
 @Component({
-    selector: 'app-game',
-    standalone: true,
-    templateUrl: './game.component.html',
-    styleUrls: ['./game.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [
-        CommonModule,
-        GameRulesComponent
-    ]
+  selector: 'app-game',
+  standalone: true,
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, GameRulesComponent],
 })
-export class GameComponent { }
+export class GameComponent {
+  input = '';
+  error = false;
+  gameStatus: 'active' | 'stopped' = 'stopped';
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    const digit = event.key;
+
+    if (this.gameStatus === 'stopped') {
+      return;
+    }
+
+    // Check if the pressed key is a digit and the next digit of π
+    if (event.key >= '0' && event.key <= '9') {
+      this.digitPressed(digit);
+    }
+  }
+
+  digitPressed(digit: string) {
+    this.input += digit;
+  }
+
+  play() {
+    this.error = false;
+    this.input = '';
+    this.gameStatus = 'active';
+  }
+}
